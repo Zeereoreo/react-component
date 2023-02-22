188005194d840f73f18c1b390b8d3ec951ebd311
Zeereoreo <wltjrcjf12@naver.com>
2023-02-22 13:05:49 +0900

    Modal 컴포넌트 완료

--- src/components/BareMinimumRequirements/Modal.js
+++ src/components/BareMinimumRequirements/Modal.js
@@ -3,12 +3,23 @@ import styled from 'styled-components';
 
 export const ModalContainer = styled.div`
   // TODO : Modal을 구현하는데 전체적으로 필요한 CSS를 구현합니다.
-  display : flex
-  justify-content: center
+  display : flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%
 `;
 
 export const ModalBackdrop = styled.div`
   // TODO : Modal이 떴을 때의 배경을 깔아주는 CSS를 구현합니다.
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+  top:0;
+  left:0;
+  right:0;
+  bottom:0;
+  background-color: rgba(0,0,0, 0.5);
 `;
 
 export const ModalBtn = styled.button`
@@ -17,7 +28,7 @@ export const ModalBtn = styled.button`
   border: none;
   padding: 20px;
   color: white;
-  border-radius: 30px;
+  border-radius: 50px;
   cursor: grab;
 `;
 
@@ -26,6 +37,18 @@ export const ModalView = styled.div.attrs((props) => ({
   role: 'dialog',
 }))`
   // TODO : Modal창 CSS를 구현합니다.
+  border-radius: 20px;
+  background-color: white;
+  width: 300px;
+  height: 150px;
+  display : flex;
+  flex-direction: column;
+  align-items: center;
+    >button {
+      color: black;
+      font-size: 1rem;
+      margin: 50px;
+    }
 `;
 
 export const Modal = () => {
@@ -43,10 +66,18 @@ export const Modal = () => {
         // TODO : 클릭하면 Modal이 열린 상태(isOpen)를 boolean 타입으로 변경하는 메소드가 실행되어야 합니다.
         onClick={openModalHandler}
         >
-          Open Modal
           {/* TODO : 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때는 ModalBtn의 내부 텍스트가 'Opened!' 로 Modal이 닫힌 상태(isOpen이 false인 상태)일 때는 ModalBtn 의 내부 텍스트가 'Open Modal'이 되도록 구현해야 합니다. */}
+          {isOpen? 'Opened!' : 'Open Modal'}
         </ModalBtn>
         {/* TODO : 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때만 모달창과 배경이 뜰 수 있게 구현해야 합니다. */}
+        {isOpen === false ? null :
+        <ModalBackdrop onClick={openModalHandler}>
+          <ModalView onClick={(event)=> event.stopPropagation()}>
+            <button onClick={openModalHandler}>x</button>
+            <div >Hello World!</div>
+          </ModalView>
+        </ModalBackdrop>
+        }
       </ModalContainer>
     </>
   );
--- src/components/BareMinimumRequirements/Toggle.js
+++ src/components/BareMinimumRequirements/Toggle.js
@@ -36,17 +36,19 @@ export const Toggle = () => {
 
   const toggleHandler = () => {
     // TODO : isOn의 상태를 변경하는 메소드를 구현합니다.
+    setisOn(!isOn)
   };
 
   return (
     <>
       <ToggleContainer
         // TODO : 클릭하면 토글이 켜진 상태(isOn)를 boolean 타입으로 변경하는 메소드가 실행되어야 합니다.
+        onClick={toggleHandler}
       >
         {/* TODO : 아래에 div 엘리먼트 2개가 있습니다. 각각의 클래스를 'toggle-container', 'toggle-circle' 로 지정하세요. */}
         {/* TIP : Toggle Switch가 ON인 상태일 경우에만 toggle--checked 클래스를 div 엘리먼트 2개에 모두 추가합니다. 조건부 스타일링을 활용하세요. */}
-        <div />
-        <div />
+        <div className='toggle-container'/>
+        <div className='toggle-circle'/>
       </ToggleContainer>
       {/* TODO : Desc 컴포넌트를 활용해야 합니다. */}
       {/* TIP:  Toggle Switch가 ON인 상태일 경우에 Desc 컴포넌트 내부의 텍스트를 'Toggle Switch ON'으로, 그렇지 않은 경우 'Toggle Switch OFF'가 됩니다. 조건부 렌더링을 활용하세요. */}
